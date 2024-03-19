752dfe67cff4e4c27356ab3b23fc90065b6e25b9
imranmashoud <aaborjona@gmail.com>
2024-03-19 10:52:57 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import "./App.css";
+// import DaisyNav from "./components/DaisyNav/DaisyNav";
+import NavBar from "./components/NavBar/NavBar";
+
+function App() {
+  return (
+    <>
+      {/* <DaisyNav></DaisyNav> */}
+      <NavBar></NavBar>
+      <h1 className="text-7xl">Vite + React</h1>
+    </>
+  );
+}
+
+export default App;
